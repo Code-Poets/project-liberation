8a134a2d6cb55316e0ae182a0186bde479bc3614
Karol Beker <karolbeker@gmail.com>
2019-07-17 12:09:58 +0200

    Substitute sqlite for postgres database

--- project_liberation/project_liberation/settings/base.py
+++ project_liberation/project_liberation/settings/base.py
@@ -22,8 +22,6 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # SECURITY WARNING: It should be located in your local_settings.py.
 # SECRET_KEY = ''
 
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
 
 ALLOWED_HOSTS = []
 
@@ -74,9 +72,14 @@ WSGI_APPLICATION = 'project_liberation.wsgi.application'
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    "default": {
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": "project_liberation",
+        "ATOMIC_REQUESTS": True,
+        # 'USER':     'postgres',
+        # 'PASSWORD': '',
+        # 'HOST':     '',
+        # 'PORT':     '',
     }
 }
 
