b1bc6382372eb7c596af7ddcbcfe755b31158741
Maciej <sam.maciej@interia.pl>
2019-09-11 17:03:12 +0200

    Add map in homepage

--- company_website/views.py
+++ company_website/views.py
@@ -1,6 +1,14 @@
+from typing import Any
+
+from django.conf import settings
 from django.views.generic import TemplateView
 
 
 class MainPageView(TemplateView):
 
     template_name = "main_page.haml"
+
+    def get_context_data(self, **kwargs: Any) -> dict:
+        context_data = super().get_context_data(**kwargs)
+        context_data["google_api_key"] = settings.GOOGLE_API_KEY
+        return context_data
