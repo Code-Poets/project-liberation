035a610cc6067de152645395ca8a7b172c22d406
Karol Beker <karolbeker@gmail.com>
2019-09-20 11:20:48 +0200

    Change project nesting level

--- lint.sh
+++ lint.sh
@@ -2,7 +2,7 @@
 
 printf "[FLAKE8: project-liberation]\n"
 flake8                                                                                          \
-    --exclude=project_liberation/project_liberation/settings/,project_liberation/manage.py,     \
+    --exclude=project_liberation/settings/,		     											\
     --jobs=4                                                                                    \
     --max-line-length=120                                                                       \
     --ignore=E124,E126,E128,E131,E156,E201,E221,E222,E241,E265,E271,E272,E701,F405,E501,W503    \
