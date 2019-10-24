6f41ccd611b62d4a49fba9838a7c12685e1296c3
Szymiks <michal.szymczak@codepoets.it>
2019-10-24 10:50:38 +0200

    Create how we work view and set path in settings

--- company_website/urls.py
+++ company_website/urls.py
@@ -5,6 +5,7 @@ from django.conf.urls.static import static
 from . import views
 
 urlpatterns = [
-    url(r"^$", views.MainPageView.as_view(), name="main_page"),
-    url(r"^team-introduction/$", views.TeamIntroductionPageView.as_view(), name="team_introduction"),
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+                  url(r"^$", views.MainPageView.as_view(), name="main_page"),
+                  url(r"^team-introduction/$", views.TeamIntroductionPageView.as_view(), name="team_introduction"),
+                  url(r"^how-we-work/$", views.HowWeWorkView.as_view(), name="how_we_work"),
+              ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
