b12a46768f24d8bbeae087c267e631d800cc8713
Karol Beker <karolbeker@gmail.com>
2019-10-31 17:02:12 +0100

    Add search articles view

--- blog/views.py
+++ blog/views.py
@@ -1,1 +1,25 @@
 # Create your views here.
+from django.shortcuts import render
+from wagtail.core.models import Page
+from wagtail.search.models import Query
+
+from blog.models import BlogArticlePage
+
+
+def search(request):
+    search_query = request.GET.get("phrase", None)
+    if search_query:
+        articles_results = BlogArticlePage.objects.live().search(search_query)
+        articles_page_ids = [p.page_ptr.id for p in articles_results]
+        search_results = Page.objects.live().filter(id__in=articles_page_ids)
+
+        query = Query.get(search_query)
+
+        # Record hit
+        query.add_hit()
+    else:
+        search_results = Page.objects.none()
+
+    return render(
+        request, "search/search_results.haml", {"search_query": search_query, "search_results": search_results}
+    )
