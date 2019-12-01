fd2452489f7de788672a9019878054b9eeacd410
Karol Beker <karolbeker@gmail.com>
2019-12-01 11:09:20 +0100

    Add blog_category_page mock tests

---
+++ blog/tests/test_blog_category_page.py
@@ -0,0 +1,29 @@
+from django.test import TestCase
+
+from blog.tests.test_helpers import BlogTestHelpers
+
+
+class TestBlogCategoryPage(TestCase, BlogTestHelpers):
+    def test_that_new_blog_category_snippet_should_create_blog_category_page(self):
+        pass
+
+    def test_that_delete_blog_category_snippet_should_delete_blog_category_page(self):
+        pass
+
+    def test_that_new_blog_category_page_should_create_blog_category_snippet(self):
+        pass
+
+    def test_that_delete_blog_category_page_should_delete_blog_category_snippet(self):
+        pass
+
+    def test_that_change_any_blog_category_snippet_parameter_should_change_blog_category_page_parameter(self):
+        pass
+
+    def test_that_change_any_blog_category_page_parameter_should_change_blog_category_snippet_parameter(self):
+        pass
+
+    def test_that_blog_category_page_can_be_only_index_blog_page_child(self):
+        pass
+
+    def test_that_new_category_page_should_has_all_mandatory_parameters(self):
+        pass
