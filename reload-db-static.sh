2da00379e4664f7edb69b458c7444ad4f105a87e
Karol Beker <karolbeker@gmail.com>
2019-12-30 14:17:04 +0100

    Add script to reseting and initializating project database

--- reload-db-static.sh
+++ reload-db-static.sh
@@ -1,6 +1,4 @@
-psql -U postgres --command="drop database project_liberation;"
-psql -U postgres --command="create database project_liberation;"
-./manage.py makemigrations
-./manage.py migrate
-./manage.py shell -c "from django.contrib.auth.models import User; User.objects.create_superuser('admin', 'admin@codepoets.it', 'superuser')"
+#!/bin/bash -e
+
+./reset-and-init-db.sh
 ./reload-statics.sh
