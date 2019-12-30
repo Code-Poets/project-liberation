4a3f905cd140bae0e1ab6240e3976aeebfd313bd
Karol Beker <karolbeker@gmail.com>
2019-12-30 10:39:41 +0100

    Fix Employees fakers

--- company_website/factories.py
+++ company_website/factories.py
@@ -1,5 +1,6 @@
+import factory
 from factory import DjangoModelFactory
-from factory import Faker
+from faker import Faker
 
 from company_website.models import Employees
 
@@ -8,10 +9,11 @@ class EmployeeFactory(DjangoModelFactory):
     class Meta:
         model = Employees
 
-    name = Faker("name")
-    position = Faker("name")
+    name = factory.Faker("first_name")
+    position = factory.LazyAttributeSequence(lambda o, n: f"{Faker().job()[:50]}")
 
 
 class BossFactory(EmployeeFactory):
 
+    name = factory.Faker("name")
     boss = True
