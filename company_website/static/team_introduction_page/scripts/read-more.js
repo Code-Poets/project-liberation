1c48cec0e789b68f6614aaa90d1a70ced875b8a4
Karol Beker <karolbeker@gmail.com>
2019-12-31 09:48:51 +0100

    Change method of animating who are we part in team introduction page

--- company_website/static/team_introduction_page/scripts/read-more.js
+++ company_website/static/team_introduction_page/scripts/read-more.js
@@ -1,6 +1,6 @@
 $(document).ready(function(){
 $(".read-more-less").click(function(){
-    $(".hidden").toggle();
-    $(".read-more").toggle();
+    $(".hidden").slideToggle();
+    $(".read-more").slideToggle();
 });
 });
