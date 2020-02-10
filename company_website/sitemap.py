7450dcdb56bf8cbaeacc3a365804999b2299df75
Jakub89 <jakub.czachura@codepoets.it>
2020-02-10 12:54:17 +0100

    Remove "career" from CompanyWebsiteViewSitemap

--- company_website/sitemap.py
+++ company_website/sitemap.py
@@ -7,7 +7,7 @@ from common.custom_sitemap import CustomSitemap
 
 class CompanyWebsiteViewSitemap(CustomSitemap):
     def items(self) -> List[str]:
-        return ["main_page", "estimate_project", "team_introduction", "how_we_work", "career", "privacy_and_policy"]
+        return ["main_page", "estimate_project", "team_introduction", "how_we_work", "privacy_and_policy"]
 
     def location(self, item: str) -> str:
         return reverse(item)
