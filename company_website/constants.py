85e548f95d8ba58632450e7dd4af58c357c64054
Maciej <sam.maciej@interia.pl>
2020-06-24 16:36:59 +0200

    [M] Change text at Estimate Project page

--- company_website/constants.py
+++ company_website/constants.py
@@ -5,8 +5,8 @@ class EstimateConstants:
     product_choices = (("build", "Build something new"), ("improve", "Improve existing project"))
     budget_choices = (
         ("-8", "up to $8.000"),
-        ("8-25", "$8.000 - $25.000"),
-        ("25-100", "$25.000 - $100.000"),
+        ("8-25", "$8.000 — $25.000"),
+        ("25-100", "$25.000 — $100.000"),
         ("100+", "$100.000+"),
     )
     project_duration_choices = (("-3", "up to 3 months"), ("3-6", "3-6 months"), ("6+", "6+ months"))
