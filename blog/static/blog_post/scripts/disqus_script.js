8cbac20897ecd3fd4bb39c17941966193c6f2442
maciejSamerdak <sam.maciej@interia.pl>
2020-07-22 23:02:40 +0200

    Remove disqus configuration script

--- blog/static/blog_post/scripts/disqus_script.js
+++
@@ -1,9 +0,0 @@
-var disqus_config = function () {
-    this.page.url = document.getElementById("blog_page_url").innerHTML;
-    };
-    (function() {
-        var d = document, s = d.createElement('script');
-        s.src = 'https://code-poets.disqus.com/embed.js';
-        s.setAttribute('data-timestamp', +new Date());
-        (d.head || d.body).appendChild(s);
-    })();
