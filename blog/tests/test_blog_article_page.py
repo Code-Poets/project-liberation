52191d94a67f2054edac192dbb1a79adfefc5f66
Maciej <sam.maciej@interia.pl>
2020-08-05 16:33:06 +0200

    Include field in the unit tests

--- blog/tests/test_blog_article_page.py
+++ blog/tests/test_blog_article_page.py
@@ -2,6 +2,7 @@ from django.core.exceptions import ValidationError
 from django.test import TestCase
 from django.utils.datetime_safe import datetime
 from parameterized import parameterized
+from wagtail.core.blocks import PageChooserBlock
 from wagtail.core.blocks import StreamBlock
 from wagtail.core.blocks import StreamValue
 from wagtailmarkdown.blocks import MarkdownBlock
@@ -27,8 +28,8 @@ class TestBlogArticlePage(TestCase, BlogTestHelpers):
     )
     def test_that_new_article_page_should_has_all_mandatory_parameters(self, parameter_name, parameter_value):
         author = BossFactory()
-        block = StreamBlock([("markdown", MarkdownBlock())])
-        body = StreamValue(block, [("markdown", "Hello, World")])
+        body_block = StreamBlock([("markdown", MarkdownBlock())])
+        body = StreamValue(body_block, [("markdown", "Hello, World")])
         blog_article_parameter = {
             "title": "Simple Article Title",
             "date": datetime.now(),
@@ -67,3 +68,42 @@ class TestBlogArticlePage(TestCase, BlogTestHelpers):
         title_that_is_way_too_long = "f" * (MAX_BLOG_ARTICLE_TITLE_LENGTH + 1)
         with self.assertRaises(ValidationError):
             self._create_blog_article_page(title=title_that_is_way_too_long)
+
+    def test_that_recommended_articles_should_not_contain_duplicates(self):
+        other_article = self._create_blog_article_page()
+        articles_block = StreamBlock([("page", PageChooserBlock())])
+        recommended_articles_with_duplicates = StreamValue(
+            articles_block, [("page", other_article), ("page", other_article)]
+        )
+        with self.assertRaises(ValidationError):
+            self._create_blog_article_page(recommended_articles=recommended_articles_with_duplicates)
+
+    def test_that_existing_article_should_not_be_given_duplicated_article_in_recommendations(self):
+        other_article = self._create_blog_article_page()
+        articles_block = StreamBlock([("page", PageChooserBlock())])
+        recommended_articles = StreamValue(articles_block, [("page", other_article)])
+        article = self._create_blog_article_page(recommended_articles=recommended_articles)
+        article.recommended_articles.stream_data.append(("page", other_article))
+        with self.assertRaises(ValidationError):
+            article.full_clean()
+
+    def test_that_article_with_no_recommended_articles_should_be_valid(self):
+        author = BossFactory()
+        body_block = StreamBlock([("markdown", MarkdownBlock())])
+        body = StreamValue(body_block, [("markdown", "Hello, World")])
+        blog_article_parameter = {
+            "title": "Simple Article Title",
+            "date": datetime.now(),
+            "intro": "Simple Article Intro",
+            "body": body,
+            "author": author,
+            "read_time": 7,
+            "views": 0,
+        }
+        articles_block = StreamBlock([("page", PageChooserBlock())])
+
+        blog_article_page = BlogArticlePage(**blog_article_parameter)
+        self.blog_index_page.add_child(instance=blog_article_page)
+        blog_article_page.save()
+
+        self.assertEqual(blog_article_page.recommended_articles, StreamValue(articles_block, []))
