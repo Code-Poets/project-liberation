885c4c4230fd811d0fafc2433914a2d97225844f
Marcin Sołek <marcin.solek@codepoets.it>
2020-11-09 15:31:40 +0100

    Add exception to external links handler script

--- company_website/static/common/scripts/external-links-handler.js
+++ company_website/static/common/scripts/external-links-handler.js
@@ -1,10 +1,15 @@
+let talkWithAdrianButtonId = "#talk-with-adrian"
+
 $('a').each(function() {
-   let local_link_expression = new RegExp('/' + window.location.host + '/');
-   if(!local_link_expression.test(this.href)) {
-       $(this).click(function(event) {
-           event.preventDefault();
-           event.stopPropagation();
-           window.open(this.href, '_blank');
-       });
-   }
+    if(!$(this).is(talkWithAdrianButtonId)) {
+        let localLinkExpression = new RegExp('/' + window.location.host + '/');
+
+        if (!localLinkExpression.test(this.href)) {
+            $(this).click(function (event) {
+                event.preventDefault();
+                event.stopPropagation();
+                window.open(this.href, '_blank');
+            });
+        }
+    }
 });
