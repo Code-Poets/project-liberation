d153b631496c3cfd58eaf0d1e6883f6ac8dd6f77
Marcin Sołek <marcin.solek@codepoets.it>
2020-11-20 16:29:09 +0100

    Create filter to text shortening

--- blog/templatetags/blog_article_page_filters.py
+++ blog/templatetags/blog_article_page_filters.py
@@ -1,3 +1,5 @@
+import textwrap
+
 from django import template
 
 from blog.models import BlogArticlePage
@@ -8,3 +10,8 @@ register = template.Library()
 @register.filter
 def get_header_id(page: BlogArticlePage, title: str) -> int:
     return page.get_header_id(title)
+
+
+@register.filter
+def shorten_text(text: str, max_length: int) -> str:
+    return textwrap.shorten(text, width=max_length, placeholder="...")
