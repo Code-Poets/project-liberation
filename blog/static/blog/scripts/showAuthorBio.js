b093c6014633df813910966aa3fcf200904ada1a
Marcin Sołek <marcin.solek@codepoets.it>
2020-12-16 16:15:22 +0100

    Add functionality to biography

---
+++ blog/static/blog/scripts/showAuthorBio.js
@@ -0,0 +1,31 @@
+const colorOfAuthorName = "#03428c"
+
+function showBio(element, authorBio){
+    if (authorBio !== "") {
+        var currentBioToDisplay = element.children[0];
+        currentBioToDisplay.style.display = "block";
+    }
+}
+
+document.body.addEventListener("click", function(evt) {
+    if (evt.target.className === 'close' || evt.target.className === "bio-modal") {
+        var bioModals = document.getElementsByClassName('bio-modal');
+        for (var elementNumber = 0; elementNumber < bioModals.length; elementNumber++ ) {
+            bioModals[elementNumber].style.display = "none";
+}
+    }
+});
+
+document.body.addEventListener("mouseover", function(evt) {
+    if (evt.target.className === 'author-name') {
+        var targetAuthor = evt.target.children[0];
+        var lengthOfAuthorBio = targetAuthor.getElementsByClassName("text")[0].innerText.trim().length;
+
+        if (lengthOfAuthorBio > 0) {
+            evt.target.style.cursor = "pointer";
+        }
+        else {
+            evt.target.style.color = colorOfAuthorName
+        }
+    }
+});
