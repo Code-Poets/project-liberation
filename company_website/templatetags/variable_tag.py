0a26de3eb4b16eb6bfcdb91fe46c6216f5d06d65
Marcin Sołek <marcin.solek@codepoets.it>
2021-01-19 15:08:22 +0100

    Register filter to check that browser is supported

--- company_website/templatetags/variable_tag.py
+++ company_website/templatetags/variable_tag.py
@@ -1,6 +1,7 @@
 from typing import Any
 
 from django import template
+from django.template.defaultfilters import stringfilter
 
 register = template.Library()
 
@@ -8,3 +9,12 @@ register = template.Library()
 @register.simple_tag
 def define_variable(value: Any = None) -> Any:
     return value
+
+
+@register.filter
+@stringfilter
+def is_webp_supported(browser_family: str) -> bool:
+    browser_family = browser_family.lower()
+    is_supported = all(["ie" not in browser_family, "safari" not in browser_family])
+
+    return is_supported
