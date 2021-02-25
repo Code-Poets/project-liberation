02a45c9efd7eb9da583293e13112de57d6605550
Marcin Sołek <marcin.solek@codepoets.it>
2021-02-25 13:19:37 +0100

    Add filters for add slash and check pagination

--- blog/templatetags/blog_article_page_filters.py
+++ blog/templatetags/blog_article_page_filters.py
@@ -15,3 +15,13 @@ def get_header_id(page: BlogArticlePage, title: str) -> int:
 @register.filter
 def shorten_text(text: str, max_length: int) -> str:
     return textwrap.shorten(text, width=max_length, placeholder="...")
+
+
+@register.filter
+def add_slash(path: str) -> str:
+    return f"{path}/" if path[-1] != "/" else path
+
+
+@register.filter
+def is_pagination_suffix(path: str) -> bool:
+    return "?page=" in path
